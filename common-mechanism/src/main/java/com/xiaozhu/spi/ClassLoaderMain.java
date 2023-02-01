7e711d849a8e93847696535d24ee8462a30eaff4
xiaohan2013 <xiaohan364@yahoo.com>
2023-02-01 10:11:19 +0800

    classloader demo

--- common-mechanism/src/main/java/com/xiaozhu/spi/ClassLoaderMain.java
+++ common-mechanism/src/main/java/com/xiaozhu/spi/ClassLoaderMain.java
@@ -1,17 +1,31 @@
 package com.xiaozhu.spi;
 
+import sun.misc.CompoundEnumeration;
+
 import java.io.IOException;
 import java.net.URL;
 import java.util.Enumeration;
 
 public class ClassLoaderMain {
+    //获取资源的方法，读取jar包中的资源文件
+//    public static Enumeration<URL> getResources(String name) throws IOException {
+//        Enumeration<URL>[] tmp = (Enumeration<URL>[]) new Enumeration<?>[2];
+//        if (parent != null) {
+//            tmp[0] = parent.getResources(name);
+//        } else {
+//            tmp[0] = getBootstrapResources(name);
+//        }
+//        tmp[1] = findResources(name);
+//        return new CompoundEnumeration<>(tmp);
+//    }
+
     public static void main(String[] args) throws IOException {
         System.out.println("======");
-//        String name = "java/sql/Array.class";
-//        Enumeration<URL> urls = Thread.currentThread().getContextClassLoader().getResources(name);
-//        while (urls.hasMoreElements()) {
-//            URL url = urls.nextElement();
-//            System.out.println(url.toString());
-//        }
+        String name = "java/sql/Array.class";
+        Enumeration<URL> urls = Thread.currentThread().getContextClassLoader().getResources(name);
+        while (urls.hasMoreElements()) {
+            URL url = urls.nextElement();
+            System.out.println(url.toString());
+        }
     }
 }
