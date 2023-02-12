e5cbb42c40d6b4b9734142ec1fd67e1be2c9838b
xiaohan2013 <xiaohan364@yahoo.com>
2023-02-12 17:55:36 +0800

    future and futuretask

--- common-mechanism/src/main/java/com/xiaozhu/async/FutureTaskMain.java
+++ common-mechanism/src/main/java/com/xiaozhu/async/FutureTaskMain.java
@@ -1,9 +1,30 @@
 package com.xiaozhu.async;
 
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.FutureTask;
+import java.util.concurrent.*;
 
 public class FutureTaskMain {
+    static class Task implements Callable<Integer> {
+        @Override
+        public Integer call() throws Exception {
+            System.out.println("Thread executing....");
+            Thread.sleep(1000);
+
+            int num = 0;
+            for(int i = 0; i < 10; i++) {
+                num += i;
+            }
+            return num;
+        }
+    }
+    public static void test() throws ExecutionException, InterruptedException {
+        ExecutorService service = Executors.newCachedThreadPool();
+        Task task = new Task();
+        FutureTask<Integer> futureTask = new FutureTask<>(task);
+        System.out.println(futureTask.get());
+        service.submit(futureTask);
+        service.shutdown();
+    }
+
     public static void main(String[] args) {
         System.out.println("Start threading.....");
         FutureTask<String> task = new FutureTask<>(()->{
@@ -12,13 +33,11 @@ public class FutureTaskMain {
 
             return "this is a callable.";
         });
-
 //        task.run();
-
         new Thread(task).start();
-
         System.out.println("Wait to get result");
         try {
+            test();
             System.out.println(task.get());
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
